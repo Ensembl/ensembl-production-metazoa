76692417a15fa1f01c65b71ad21f4359966d85e2
Vasily Sitnik <vsitnik@ebi.ac.uk>
2023-06-19 14:29:56 +0100

    using python3.9 for metazoan env

--- scripts/checkout_ensembl.20210208.sh
+++ scripts/checkout_ensembl.20210208.sh
@@ -160,8 +160,8 @@ git clone git@github.com:Ensembl/ensembl-production-imported-private.git || true
 # prepare ensembl-genomio
 echo "Building python3 venv" >> /dev/stderr
 
-# pyenv local 3.7.6
-python3 -m venv venv
+# pyenv local 3.9
+python3.9 -m venv venv
 source venv/bin/activate
 pip3 install Cython
 
